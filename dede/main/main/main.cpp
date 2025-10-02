95c50f5eb0edd02d08ef167ef3ad6272755a95d8
Sadd1DK <kushnirov07@mail.ru>
2025-10-02 16:25:18 +0300

    Add files via upload

---
+++ dede/main/main/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int X;
+
+    cout << "Enter the number x (not 0): ";
+    cin >> X;
+
+    if (X == 0) {
+        cout << "X cannot be 0" << endl;
+        return 1;
+    }
+
+    // Вычисление обратного числа
+    double reciprocal = 1.0 / X;
+
+    // Вычисление степеней
+    int square = pow(X, 2);
+    int fifth_power = pow(X, 5);
+
+    cout << "Reverse number: " << reciprocal << endl;
+    cout << "x^2: " << square << endl;
+    cout << "x^5: " << fifth_power << endl;
+
+    return 0;
+}
